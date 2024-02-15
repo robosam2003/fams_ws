aaaa02fa0b7e43e1c3965bb7cb77b2c4d8b20402
BenjHarv <bdsharvey1@sheffield.ac.uk>
2024-02-15 14:15:18 +0000

    update

--- src/machine_vision/machine_vision/calibrator.py
+++ src/machine_vision/machine_vision/calibrator.py
@@ -14,21 +14,21 @@ calibrate_path = '/home/uos/fams_ws/src/machine_vision/machine_vision/Calibrate'
 images = os.listdir(calibrate_path)
 for fname in images:
     print(fname)
-    path = os.join(calibrate_path, fname)
+    path = os.path.join(calibrate_path, fname)
     img = cv.imread(path)
-    window_name='window'
-    cv.imshow(window_name,img)
-    cv.waitkey(500)
-    # gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    # # Find the chess board corners
-    # ret, corners = cv.findChessboardCorners(gray, (7,7), None)
-    # # If found, add object points, image points (after refining them)
-    # if ret == True:
-    #     objpoints.append(objp)
-    #     corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
-    #     imgpoints.append(corners2)
-    #     # Draw and display the corners
-    #     cv.drawChessboardCorners(img, (7,6), corners2, ret)
-    #     cv.imshow('img', img)
-    #     cv.waitKey(500)
+
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, (7,7), None)
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+        imgpoints.append(corners2)
+        # Draw and display the corners
+        cv.drawChessboardCorners(img, (7,7), corners2, ret)
+        cv.imshow('img', img)
+        cv.waitKey(2000)
 cv.destroyAllWindows()
+
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
