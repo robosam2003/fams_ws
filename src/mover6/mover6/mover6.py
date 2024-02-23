a2046262749929888b703f229698f9cb80d47215
robosam2003 <robosam2003@gmail.com>
2024-02-23 12:37:44 +0000

    fix overcurrent bug

--- src/mover6/mover6/mover6.py
+++ src/mover6/mover6/mover6.py
@@ -7,14 +7,14 @@ def _get_message(msg):
     return msg
 
 ERROR_DICT = {
-    0x01: "Brown Out or Watch Dog",
-    0x02: "VELOCITY LAG",
-    0x04: "MOTOR NOT ENABLED",
-    0x08: "COMM WATCHDOG",
-    0x10: "POSITION LAG",
-    0x20: "ENCODER ERROR",
-    0x30: "OVER CURRENT",
-    0x40: "CAN ERROR",
+    0x01: "Brown Out or Watch Dog",  # 0b00000001
+    0x02: "VELOCITY LAG", # 0b00000010
+    0x04: "MOTOR NOT ENABLED", # 0b00000100
+    0x08: "COMM WATCHDOG", # 0b00001000
+    0x10: "POSITION LAG", # 0b00010000
+    0x20: "ENCODER ERROR",   # 0b00100000
+    0x40: "OVER CURRENT", # 0b01000000
+    0x80: "CAN ERROR", # 0b10000000
 }
 
 
@@ -25,9 +25,11 @@ class RecMsg():
         self.data = msg.data
         self.error_code = self.data[0]
         self.error = ""
+        
+        # For each error code, one of the 8 bits is set.
         for key in ERROR_DICT:
             if self.error_code & key:
-                self.error = self.error + "\n" + ERROR_DICT[key]
+                self.error += "\n" + ERROR_DICT[key]
         self.velocity = self.data[1]
         self.position = self.data[2] + (self.data[3] << 8) - 32768
         self.shunt = self.data[4]
@@ -35,8 +37,11 @@ class RecMsg():
         self.div_value = self.data[6]
         self.digital_input = self.data[7]
 
+ 
     def __str__(self):
-        return f"ID: {self.id}, Velocity: {self.velocity}, Position: {self.position}"
+        data = [hex(i) for i in self.data]
+        data_str = " ".join(data)
+        return f"ID: {hex(self.id)}, Velocity: \033[34m{self.velocity}\033[0m, Position: \033[32m{self.position}\033[0m, Data: {data_str}"
 
 
 class mover6():
@@ -56,21 +61,47 @@ class mover6():
 
 
     def main_loop(self):
+        max_joint = 1
         # 2. Start the main loop and send position command cyclically 
+        for joint in range(1, max_joint+1):
+            self.joint_to_position(joint, self.joint_positions[joint-1])
+            rec_msg = self.recv()
+            # Parse the message and update the joint position
+            joint_pos = rec_msg.position
+            time.sleep(2/1000) # When 
+            self.joint_positions[joint-1] = joint_pos
+
+        # 3. Reset all joints: 
+        for joint in range(1, max_joint+1):
+            self.reset_axis(joint)
+            time.sleep(2/1000)
+        # 4. Enable all joints
+        for joint in range(1, max_joint+1):
+            self.enable_axis(joint)
+            time.sleep(2/1000)
+        
         while True:
-            for joint in range(1, 7):
-                self.joint_to_position(joint, self.joint_positions[joint-1])
+            for joint in range(1, max_joint+1):
+                # for all the joints, slowly move to the zero position
+                angle_command = 32768
+                if self.joint_positions[joint-1] > 32768:
+                    angle_command = self.joint_positions[joint-1] - 50
+                elif self.joint_positions[joint-1] < 32768:
+                    angle_command = self.joint_positions[joint-1] + 50
+            
+                # angle_command = self.joint_positions[joint-1]
+                self.joint_to_position(joint, angle_command)
                 rec_msg = self.recv()
                 # Parse the message and update the joint position
                 joint_pos = rec_msg.position
-
-
                 time.sleep(2/1000) # When 
+                self.joint_positions[joint-1] = joint_pos
+            time.sleep(0.05) # 20Hz
 
 
     def joint_to_position(self, joint_no, pos_deg):
         id = joint_no*16  # (it's in hex)
-        vel = 100  # 127 is zero
+        vel = 130  # 127 is zero
         # break the position into two bytes
         pos = 32768 + int(pos_deg*int(32786/180))
         pos_low = pos & 0xff
@@ -82,9 +113,8 @@ class mover6():
         # print(f"Sent: {data}")
         print("Set Joint", joint_no, "to position: ", pos_deg)
 
-    def joint_to_velocity(self, joint_no, vel):
-        id = joint_no*16
-        # break the velocity into two bytes
+
+
 
 
 
@@ -94,23 +124,34 @@ class mover6():
         self.bus.send(message)
 
     def recv(self):
-        rec_msg = RecMsg(self.bus.recv())
+        msg = self.bus.recv()
+        rec_msg = RecMsg(msg)
 
         if rec_msg.error != "":
             print(f"\033[91mError: {rec_msg.error}\033[0m")
         print(f"Received: {rec_msg}")
         return rec_msg
     
+    def reset_axis(self, axis_no):
+        print("Resetting axis", axis_no, "...")
+        id = axis_no*16
+        data = [0x01, 0x06]
+        self.send(id, data)
+        time.sleep(0.1)
+        # Acknowledge the reset
+        # rec_msg = self.recv()
+    
     def enable_axis(self, axis_no):
         id = axis_no*16 # (it's in hex)
         data = [0x01, 0x09]
         self.send(id, data)
+        print("Enabled axis", axis_no)
+
 
     def enable_all_axes(self):
         for i in range(1, 7):
             self.enable_axis(i)
-            print("Enabled axis", i)
-            time.sleep(0.01)
+            time.sleep(2/1000)
 
     def shutdown(self):
         self.bus.shutdown()
--- src/workstation/workstation/workstation.py
+++ src/workstation/workstation/workstation.py
@@ -5,5 +5,3 @@ from rclpy.node import Node
 from fams_interfaces.msg import Job, SubProcess, Part, SystemState, Workstation
 
 
-class WorkstationNode(Node):
-    
