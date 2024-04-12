75bc61d521e35cf6d64950ee74c7879d2472fad6
khoobo15 <apartab1@sheffield.ac.uk>
2024-04-12 15:55:50 +0100

    Initial code of pi node

--- src/pi_mobile_control/pi_mobile_control/pi_mobile_control.py
+++ src/pi_mobile_control/pi_mobile_control/pi_mobile_control.py
@@ -18,16 +18,52 @@ class pi_control(Node):
         # Create a subscriber that subscribes to the cmd_vel topic
         self.cmd_vel_subscription = self.create_subscription(Twist, 'cmd_vel', self.cmd_vel_callback, 10)
 
+        #Initiate Serial communication to arduino
+        self.ser = serial.Serial('/dev/ttyACM0', 9600)
+
+        # Create record of start time of system
+        self.initial_time = self.get_clock().now().nanoseconds / 1e9
+
         # Initialize the joint states for rviz
         self.wheel_states = JointState()
         self.wheel_states.name = ['upper_left_wheel_joint', 'upper_right_wheel_joint', 'lower_left_wheel_joint', 'lower_right_wheel_joint']
         self.wheepi_control_logger().info('pi_control node has been started')
 
     def cmd_vel_callback(self, msg):
-        
+
         #Calculate the linear and angular velocities
         linear_velocity = msg.linear.x # This is the linear velocity in m/s
         angular_velocity = msg.angular.z  # This is the angular velocity in rad/s
+
+        wheel_radius = 0.05
+        wheel_separation = 0.3
+
+        current_time = self.get_clock().now().nanoseconds / 1e9 # This is the current time in seconds
+        elapsed_time = float(current_time - self.initial_time)
+
+        #if elapsed_time > 0.1:
+        #    elapsed_time = 0.01
+
+        # Compute the left and right wheel velocities
+        left_wheel_velocity = (linear_velocity - angular_velocity * wheel_separation / 2.0) / wheel_radius
+        right_wheel_velocity = -(linear_velocity + angular_velocity * wheel_separation / 2.0) / wheel_radius
+        # Send to Arduino
+        # Encode into some format
+        # Send via sel.ser.write?
+        
+        # Compute the left and right wheel positions
+        left_wheel_position = self.wheel_states.position[0] + left_wheel_velocity * elapsed_time
+        right_wheel_position = self.wheel_states.position[1] + right_wheel_velocity * elapsed_time
+
+        # Update the joint states
+        self.wheel_states.position = [left_wheel_position, right_wheel_position, left_wheel_position, right_wheel_position]
+        self.wheel_states.velocity = [left_wheel_velocity, right_wheel_velocity, left_wheel_velocity, right_wheel_velocity]
+
+        # Publish the joint states - this will make the wheels spin
+        self.wheel_states_publisher.publish(self.wheel_states)
+
+
+
 
 
 # Define the main function
