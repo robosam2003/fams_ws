2e579394bb496ef517f4fe0d7a309f826b82d56d
robosam2003 <robosam2003@gmail.com>
2024-04-17 19:38:02 +0100

    continue workstation controller work - vision loop and amr distance checking

--- src/workstation/workstation/workstation_controller.py
+++ src/workstation/workstation/workstation_controller.py
@@ -2,9 +2,11 @@
 from std_msgs.msg import String
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Pose
-from fams_interfaces.msg import Job, SubProcess, Part, SystemState, Workstation, Schedule, Location
+from geometry_msgs.msg import Pose, Point
+from fams_interfaces.msg import Job, SubProcess, Part, SystemState, Workstation, Schedule, Location, Vision, WorkstationCommand
 from rosidl_runtime_py import *
+import math
+import numpy as np
 
 
 class WorkstationController(Node):
@@ -14,15 +16,22 @@ class WorkstationController(Node):
         super().__init__('workstation_controller')
         self.get_logger().info('Workstation controller node has been started')
 
+        self.declare_parameters(
+            namespace='',
+            parameters=[
+                ('amr_names', ['nexus1', 'nexus2']),
+            ]
+        )
+
+        # Get the names of the AMRs from the parameter server
+        self.amr_names = self.get_parameter('amr_names').value
 
         # Docking poses for the workstations
-        w1_docking_pose = Pose()
-        w1_docking_pose.position.x, w1_docking_pose.position.y, w1_docking_pose.position.z = 0, 0, 0
-        w1_docking_pose.orientation.x, w1_docking_pose.orientation.y, w1_docking_pose.orientation.z, w1_docking_pose.orientation.w = 0, 0, 0, 0
+        w1_docking_pose = Point()
+        w1_docking_pose.position.x, w1_docking_pose.position.y, w1_docking_pose.position.z = 1, 1, 0 # x, y, yaw
 
-        w2_docking_pose = Pose()
-        w2_docking_pose.position.x, w2_docking_pose.position.y, w2_docking_pose.position.z = 0, 0, 0
-        w2_docking_pose.orientation.x, w2_docking_pose.orientation.y, w2_docking_pose.orientation.z, w2_docking_pose.orientation.w = 0, 0, 0, 0
+        w2_docking_pose = Point()
+        w2_docking_pose.position.x, w2_docking_pose.position.y, w2_docking_pose.position.z = 1, 2, math.pi # x, y, yaw
         self.workstation_docking_poses = [
             w1_docking_pose,
             w2_docking_pose
@@ -50,10 +59,41 @@ class WorkstationController(Node):
             self.schedule_handler,
             10
         )
+
+        self.vision_locations_subscriber = self.create_subscription(
+            Vision,
+            'vision_locations', # Consider naming to something better - maybe 'amr_vision_locations'
+            self.vision_locations_handler,
+            10 
+        )
         
         # Attributes that get updated
         self.system_state = SystemState()
         self.schedule = Schedule()
+        self.vision_locations = Vision()
+
+    def vision_locations_handler(self, msg):
+        # Update local vision locations attribute
+        self.vision_locations = msg
+        amr_locations = location_array_2D=np.reshape(self.vision_locations.part_location,(int(len(self.vision_locations.part_location)/3),3))
+
+        # Check if the AMRs are close enough to any of the docking poses
+        for i, amr_location in enumerate(amr_locations):
+            for j, docking_pose in enumerate(self.workstation_docking_poses):
+                distance = math.sqrt((amr_location[0] - docking_pose.position.x)**2 + (amr_location[1] - docking_pose.position.y)**2)
+                if distance < 0.1 and amr_location[2] - docking_pose.position.z < 0.1:
+                    # The AMR is close enough and aligned with the docking pose of the workstation
+                    parts = self.schedule.parts
+                    subprocesses = self.schedule.subprocesses
+                    workstations = self.schedule.workstations
+
+    
+    def send_workstation_command(self, amr_index, workstation_index):  
+        # Create a Workstation message
+        workstation_command = WorkstationCommand()
+        workstation_command.command = 'DOCK'
+        
+
     
     def system_state_handler(self, msg):
         # Update local system state attribute
@@ -71,6 +111,8 @@ class WorkstationController(Node):
 
         # Should we wait until the robot is in position? or should we have an eternal loop that check it, 
         #  and calls this function again? or another function that generates the workstationCommand?
+
+
 
 
 
